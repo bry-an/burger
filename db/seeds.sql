6186d18f397b63334dc71e28a833e2ba6d064fc0
bry-an <bbyunis@gmail.com>
2018-09-15 23:00:53 -0600

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES 
+    ('Bean Burger', false), 
+    ('Chickpea Burger', true), 
+    ('Portobello Burger', true);
---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
